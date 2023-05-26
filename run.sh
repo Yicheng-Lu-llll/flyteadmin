a2ba8c7c201b05e8b92e1fb6e410675bc2930155
Yicheng-Lu-llll <luyc58576@gmail.com>
2023-05-26 22:55:35 +0000

    develop
    Signed-off-by: Yicheng-Lu-llll <luyc58576@gmail.com>

--- run.sh
+++ run.sh
@@ -6,16 +6,14 @@ git push
 current_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 
 if [ -z "$current_tag" ]; then
-    current_tag=1
+    current_tag=v1.0.0
 fi
 
 git tag -d $current_tag
 git push origin :refs/tags/$current_tag
 
 
-new_tag=$((current_tag + 1))
-echo !!!!!!!!!!!!
-echo $new_tag
-echo !!!!!!!!!!!!
+new_tag=v1.0.1
+
 git tag $new_tag
 git push origin $new_tag
