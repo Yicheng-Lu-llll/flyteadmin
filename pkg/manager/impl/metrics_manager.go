d350f9b3f8cd300740e9307de70e391addf70669
Yicheng-Lu-llll <luyc58576@gmail.com>
2023-05-29 02:39:04 +0000

    develop
    Signed-off-by: Yicheng-Lu-llll <luyc58576@gmail.com>

--- pkg/manager/impl/metrics_manager.go
+++ pkg/manager/impl/metrics_manager.go
@@ -692,7 +692,7 @@ func (m *MetricsManager) addTimeItSpans(ctx context.Context, span *core.Span) {
 func (m *MetricsManager) getTimeItSpans(ctx context.Context, taskId *core.TaskExecutionIdentifier) []*core.Span {
 
 	fmt.Println("I am going to call db")
-	fmt.Printf("Type of m.db: %T\n", m.db)
+	fmt.Printf("Type of m.db: %T\n", m.db.TaskExecutionRepo())
 
 	// m.db.taskExecutionRepo
 
